62f86f3e6d781f6fb09586059387cf2fa3700c58
Charles McQuain <charlesmcquain@Charless-MBP.attlocal.net>
2024-05-15 13:38:11 -0500

    Added Bird model and animations

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,9 +5,11 @@ import Island from '../models/Island'
 import Sky from '../models/Sky'
 import Bird from '../models/Bird'
 import Plane from '../models/Plane'
+import HomeInfo from '../components/HomeInfo'
 
 const Home = () => {
   const [isRotating, setIsRotating] = useState(false);
+  const [currentStage, setCurrentStage] = useState(1);
 
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
@@ -41,9 +43,9 @@ const Home = () => {
 
   return (
     <section className="w-full h-screen relative">
-      {/*<div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
-        POPUP
-  </div>*/}
+      <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+       { currentStage && <HomeInfo currentStage={currentStage} />}
+      </div>
       <Canvas 
         className={`w-full h-screen bg-transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
         camera={{ near: 0.1, far: 1000}}>
@@ -53,8 +55,8 @@ const Home = () => {
           <ambientLight intensity={0.5} />
           <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
           <Bird />
-          <Sky />
-          <Island position={islandPosition} scale={islandScale} rotation={islandRotation} isRotating={isRotating} setIsRotating={setIsRotating} />
+          <Sky isRotating={isRotating} />
+          <Island position={islandPosition} scale={islandScale} rotation={islandRotation} isRotating={isRotating} setIsRotating={setIsRotating} setCurrentStage={setCurrentStage} />
           <Plane isRotating={isRotating} planeScale={planeScale} planePosition={planePosition} rotation={[0, 20, 0]}/>
         </Suspense>
       </Canvas>
