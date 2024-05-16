a806dea148eea3e215ab5e3a76f6848a2b56e6d5
Charles McQuain <charlesmcquain@gmail.com>
2024-05-15 20:25:56 -0500

    updated config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/threejs_sample/',
   plugins: [react()],
-  assetsInclude: ['**/*.glb'],
-  base: '/<REPO>/'
+  assetsInclude: ['**/*.glb']
 })
